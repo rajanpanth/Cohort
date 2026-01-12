4d738229f4669a33d154b665dbee3ed490b1d7ee
rajanpanth <rajan.pantha@samriddhicollege.edu.np>
2026-01-12 15:53:00 +0545

    auth

--- week-5/authenticstion.js
+++ week-5/authenticstion.js
@@ -7,16 +7,16 @@ app.use(express.json())
 const users = [];
 
 //returns a randon wrong string
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
+// }
 
 app.post("/signup", function (req, res) {
     const username = req.body.username;
@@ -38,21 +38,9 @@ app.post("/signup", function (req, res) {
 })
 
 app.post("/signin", function (req, res) {
-
     const username = req.body.username;
     const password = req.body.password;
-
-    // const user = users.find(function (u) {
-    //     if (u.username == username && u.password == password ) {
-    //         return true;
-    //     }
-    //     else {
-    //         return false;
-    //     }
-    // })
-
     let foundUser = null;
-
     for (let i = 0; i < users.length; i++) {
         if (users[i].username === username && users[i].password === password) {
             foundUser = users[i];
@@ -79,31 +67,31 @@ app.post("/signin", function (req, res) {
 
 })
 
-app.get("/me", function(req, res){
-   const token = req.headers.token;
-   const decodedInformation = jwt.verify(token, JWT_SECRET) //{username: pantharajan0@gmail.com}
-const username = decodedInformation.username;
+app.get("/me", function (req, res) {
+    const token = req.headers.token;
+    const decodedInformation = jwt.verify(token, JWT_SECRET) //{username: pantharajan0@gmail.com}
+    const username = decodedInformation.username;
 
-   let foundUser = null;
-   
-   for(let i = 0; i<users.length; i++){
-    if(users[i].username == username){
-        foundUser = users[i]
+    let foundUser = null;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username == username) {
+            foundUser = users[i]
+        }
     }
-   }
 
-   if(foundUser){
-    res.json({
-        username: foundUser.username,
-        password: foundUser.password
-    })
+    if (foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
 
-   }
-   else{
-    res.json({
-        msg: "user not found",
-    })
-   }
+    }
+    else {
+        res.json({
+            msg: "user not found",
+        })
+    }
 })
 
 app.listen(3000)
