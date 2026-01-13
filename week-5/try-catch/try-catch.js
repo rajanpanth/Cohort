bc0ca2a541605b40f45c4869b8df409e491a69ce
rajanpanth <rajan.pantha@samriddhicollege.edu.np>
2026-01-13 18:46:18 +0545

    jwt

---
+++ week-5/try-catch/try-catch.js
@@ -0,0 +1,11 @@
+getLength= (name) =>{
+return name.length
+}
+try{
+    const and = getLength();
+    console.log(ans);
+    console.log("nice")
+} catch(e){
+console.log("inside catch statement")
+}
+console.log("hi there.")
