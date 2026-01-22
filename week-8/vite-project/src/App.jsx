d16890a862c5f6c83a65a40a147d90349a6a329d
rajanpanth <rajan.pantha@samriddhicollege.edu.np>
2026-01-22 22:28:30 +0545

    ch

--- week-8/vite-project/src/App.jsx
+++ week-8/vite-project/src/App.jsx
@@ -1,33 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCount] = useState(0);
+  let [counterVisible, setcounterVisible] = useState(true);
+  useEffect(function () {
+    setInterval(function () {
+      setcounterVisible(function (c) {
+        return c = !c
+      })
+    }, 5000)
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <b>Counter</b>
+      {counterVisible ? <Counter></Counter> : null}
+
+    </>
+  )
+}
+
+//counter
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [timer, setTimer] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decreaser() {
+    setCount(count - 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+  //timer
+  //hooking in to life cycle event of react 
+  // mounting, re-rendering, unmounting 
+  useEffect(function () {
+    console.log("on mount")
+    let clock = setInterval(function () {
+      // setTimer(timer => timer + 1)
+      console.log("From inside setInterval")
+      setTimer(function (timer) {
+        return timer + 1
+      })
+    }, 1000)
+
+    return function () {
+      console.log("on umount")
+      clearInterval(clock)
+    }
+
+  }, [])
+
+
+
+
+
+  return (
+    <>
+      <h1>
+        {count}
+      </h1>
+      <button onClick={increment}>Increaser</button>
+      <button onClick={decreaser}>Decreaser</button>
+      <button onClick={reset}>Reset</button>
+      <br></br>
+      <h1>
+        {timer}
+      </h1>
     </>
   )
 }
